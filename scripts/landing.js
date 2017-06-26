07269d203c7ee6478ae7920be9842f719dec4f4f
xieliming2017 <xieliming2006@hotmail.com>
2017-06-26 00:19:43 -0400

    Checkpoint 29 DOM:Scripting completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-          var points = document.getElementsByClassName('point');
+var animatePoints = function(points){
 
           var revealPoint = function(index) {
               points[index].style.opacity = 1;
@@ -15,5 +15,17 @@ var animatePoints = function() {
           }
       };
 
+window.onload = function(){
 
-animatePoints();
+if(window.innerHeight > 950){
+  animatePoints(pointsArray);
+}
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll',function(event){
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance){
+      animatePoints(pointsArray);
+    }
+  });
+}
